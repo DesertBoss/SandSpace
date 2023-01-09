9389870a75bcaf855855d505bdbdf7bc5daa4f02
DesertBoss <demon050797@mail.ru>
2023-01-10 01:59:53 +0200

    Base structure

---
+++ SandSpaceMod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityModManagerNet;
+
+namespace SandSpace
+{
+	public class SandSpaceMod
+	{
+		public void Load ()
+		{
+			
+		}
+	}
+}
---
+++ Settings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace SandSpace
+{
+	public class Settings : UnityModManager.ModSettings, IDrawable
+	{
+		[Header ("Test"), Space (10f), Draw ("Test")]
+		public bool test = false;
+
+		public override void Save (UnityModManager.ModEntry modEntry)
+		{
+			UnityModManager.ModSettings.Save<Settings> (this, modEntry);
+		}
+
+		public void OnChange ()
+		{
+			
+		}
+
+		public override string GetPath (UnityModManager.ModEntry modEntry)
+		{
+			return Path.Combine (modEntry.Path, "Settings.xml");
+		}
+	}
+}
