20e63d137815d19299ce9a4c8eda624890e67408
DesertBoss <demon050797@mail.ru>
2023-01-10 02:01:29 +0200

    Hangar limit patch

---
+++ HangarsPatch.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using HarmonyLib;
+
+namespace SandSpace
+{
+	// SymbolExtensions
+	// Transpilers
+
+	internal class HangarsPatch
+	{
+		[HarmonyPatch (typeof (HangarConfig)), HarmonyPatch ("DisplayAllHangars")]
+		private static class HangarConfig_DisplayAllHangars_Patch
+		{
+			private static IEnumerable<CodeInstruction> Transpiler (IEnumerable<CodeInstruction> instructions)
+			{
+				var codes = new List<CodeInstruction>(instructions);
+
+				for (var i = 0; i < codes.Count; i++)
+				{
+					if (codes[i].opcode == OpCodes.Ldloc_0 &&
+						codes[i + 1].opcode == OpCodes.Ldc_I4_4 &&
+						codes[i + 2].opcode == OpCodes.Blt)
+					{
+						codes[i + 1].opcode = OpCodes.Ldc_I4;
+						codes[i + 1].operand = 8;
+					}
+				}
+
+				return codes.AsEnumerable ();
+			}
+		}
+
+		[HarmonyPatch (typeof (GameManager)), HarmonyPatch ("IsHangarUnlocked")]
+		private static class GameManager_IsHangarUnlocked_Patch
+		{
+			private static void Postfix (ref bool __result, ref int hangarIndex)
+			{
+				Core playerCore = StarmapManager.GetLevelSetup().GetPlayerCore();
+
+				if (hangarIndex > 3 && playerCore.HasPerk (PerkType.StrikeCraftActive_4))
+				{
+					__result = true;
+				}
+			}
+		}
+
+		[HarmonyPatch (typeof (GameManager)), HarmonyPatch ("GetHangarDisplayLevel")]
+		private static class GameManager_GetHangarDisplayLevel_Patch
+		{
+			private static void Postfix (ref int __result, ref int hangarIndex)
+			{
+				Core playerCore = StarmapManager.GetLevelSetup().GetPlayerCore();
+
+				if (hangarIndex > 3)
+				{
+					int myUnlockLevelInf = GameManager.GetPerkManager().GetPerk(PerkType.StrikeCraftActive_4).myUnlockLevel;
+					if (myUnlockLevelInf > playerCore.GetCurrentLevel ())
+					{
+						__result = myUnlockLevelInf;
+					}
+				}
+			}
+		}
+	}
+}
