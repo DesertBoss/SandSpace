103c439fdab14b5124d5c273dd7d332b40631b3b
DesertBoss <demon050797@mail.ru>
2023-01-10 16:46:32 +0200

    More readeble style

--- Settings.cs
+++ Settings.cs
@@ -10,28 +10,54 @@ namespace SandSpace
 {
 	public class Settings : UnityModManager.ModSettings, IDrawable
 	{
+		//┣┏┗┃━
+		//╠╔╚║═
 		public bool _newGameNeed = true;
 
-		[Header ("Hangars"), Space (25f)]
-
-		[Draw ("Max active hangars", Min = 1, Max = 20), Space (10f)]
+		[Header ("┏ Hangars")]
+		
+		[Draw ("┣━ Max active hangars", Min = 1, Max = 20)]
 		public int maxActiveHangars = 4;
 
-		[Draw ("Hangar 1 unlock level", Min = 0, Max = 100, InvisibleOn = "_newGameNeed|true"), Space (10f)]
+		[Draw ("┣━ Hangar 1 unlock level", Min = 0, Max = 100, InvisibleOn = "_newGameNeed|true")]
 		public int hangar_1_unlockLevel = 0;
 
-		[Draw ("Hangar 2 unlock level", Min = 0, Max = 100, InvisibleOn = "_newGameNeed|true"), Space (10f)]
+		[Draw ("┣━ Hangar 2 unlock level", Min = 0, Max = 100, InvisibleOn = "_newGameNeed|true")]
 		public int hangar_2_unlockLevel = 0;
 
-		[Draw ("Hangar 3 unlock level", Min = 0, Max = 100, InvisibleOn = "_newGameNeed|true"), Space (10f)]
+		[Draw ("┣━ Hangar 3 unlock level", Min = 0, Max = 100, InvisibleOn = "_newGameNeed|true")]
 		public int hangar_3_unlockLevel = 0;
 
-		[Draw ("Hangar 4 unlock level", Min = 0, Max = 100, InvisibleOn = "_newGameNeed|true"), Space (10f)]
+		[Draw ("┗━ Hangar 4 unlock level", Min = 0, Max = 100, InvisibleOn = "_newGameNeed|true")]
 		public int hangar_4_unlockLevel = 0;
 
-		[Space (15f), Header ("Misc"), Space (25f)]
 
-		[Draw ("Write default values when starting a new game"), Space (10f)]
+		[Header ("┣━ Infinite Perks"), Space (25f)]
+		[Header ("┏ Perks"), Space (25f)]
+		[Draw ("┣━━ Health perk bonus", Min = 1, Max = 1000)]
+		public int perkHealthInf = 25;
+
+		[Draw ("┣━━ Armor perk bonus", Min = 1, Max = 1000)]
+		public int perkArmorInf = 20;
+
+		[Draw ("┣━━ Capacitor perk bonus", Min = 1, Max = 1000)]
+		public int perkCapacitorInf = 15;
+
+		[Draw ("┣━━ Reactor perk bonus", Min = 1, Max = 1000)]
+		public int perkReactorInf = 15;
+
+		[Draw ("┣━━ Weapon Damage perk bonus", Min = 1, Max = 1000)]
+		public int perkWeaponDamageInf = 15;
+
+		[Draw ("┣━━ Shield Strength perk bonus", Min = 1, Max = 1000)]
+		public int perkShieldStrengthInf = 20;
+
+		[Draw ("┗━━ Strike Craft Reserve perk bonus", Min = 1, Max = 1000)]
+		public int perkStrikeCraftReserveInf = 8;
+
+		[Space (15f), Header ("┏ Misc"), Space (25f)]
+
+		[Draw ("┗━ Write default values when starting a new game")]
 		public bool writeDefOnNewGame = true;
 
 		public override void Save (UnityModManager.ModEntry modEntry)
