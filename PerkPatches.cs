dc3f0a192f09981f85f50e7548df4aa19dba0363
DesertBoss <demon050797@mail.ru>
2023-01-10 04:40:45 +0200

    Perk patch fix

--- PerkPatches.cs
+++ PerkPatches.cs
@@ -8,25 +8,7 @@ namespace SandSpace
 {
 	internal class PerkPatches
 	{
-		[HarmonyPatch (typeof (StarmapSetup)), HarmonyPatch ("SetupNewStarmap")]
-		private static class StarmapSetup_SetupNewStarmap_Patch
-		{
-			private static void Postfix ()
-			{
-				PerkOverride ();
-			}
-		}
-
-		[HarmonyPatch (typeof (StarmapSetup)), HarmonyPatch ("OnLoadGame_2")]
-		private static class StarmapSetup_OnLoadGame_2_Patch
-		{
-			private static void Postfix ()
-			{
-				PerkOverride ();
-			}
-		}
-
-		private static void PerkOverride ()
+		internal static void PerkOverride ()
 		{
 			var man = GameManager.GetPerkManager ();
 			man.GetPerk (PerkType.Health_Inf).myPerkValue = 0.25f;
@@ -36,6 +18,21 @@ namespace SandSpace
 			man.GetPerk (PerkType.WeaponDamage_Inf).myPerkValue = 0.15f;
 			man.GetPerk (PerkType.Shield_Strength_Inf).myPerkValue = 0.2f;
 			man.GetPerk (PerkType.StrikeCraftReserve_Inf).myPerkValue = 8f;
+
+			man.GetPerk (PerkType.StrikeCraftActive_1).myUnlockLevel = SandSpaceMod.Settings.hangar_1_unlockLevel;
+			man.GetPerk (PerkType.StrikeCraftActive_2).myUnlockLevel = SandSpaceMod.Settings.hangar_2_unlockLevel;
+			man.GetPerk (PerkType.StrikeCraftActive_3).myUnlockLevel = SandSpaceMod.Settings.hangar_3_unlockLevel;
+			man.GetPerk (PerkType.StrikeCraftActive_4).myUnlockLevel = SandSpaceMod.Settings.hangar_4_unlockLevel;
+		}
+
+		internal static void SetDefaults ()
+		{
+			var man = GameManager.GetPerkManager ();
+
+			SandSpaceMod.Settings.hangar_1_unlockLevel = man.GetPerk (PerkType.StrikeCraftActive_1).myUnlockLevel;
+			SandSpaceMod.Settings.hangar_2_unlockLevel = man.GetPerk (PerkType.StrikeCraftActive_2).myUnlockLevel;
+			SandSpaceMod.Settings.hangar_3_unlockLevel = man.GetPerk (PerkType.StrikeCraftActive_3).myUnlockLevel;
+			SandSpaceMod.Settings.hangar_4_unlockLevel = man.GetPerk (PerkType.StrikeCraftActive_4).myUnlockLevel;
 		}
 	}
 }
